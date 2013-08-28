df2da482fe271e374222ef76fd73dfd88da49735
gcDataTechnology <gcDataTechnology@gcDataTechnolog>
2013-08-27 22:25:16 -0500

    4-concrete finished employees, finished hourly

--- src/concrete/Employee.java
+++ src/concrete/Employee.java
@@ -12,12 +12,27 @@ public class Employee {
     
     public static int employeeCount;//number of employees
     
+    //individual and unique to each employee
     private String ssNumber;
-    private String name;//individual and unique to each employee
+    private String name;
+    private char gender;
+    private String hireDate;
+    private String endDate;
+    
+    //may be the same as another employee
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+    
+   
     
     
+    //Constructors
     public Employee(){
         Employee.employeeCount++;
+        
     }
     
     public Employee(String name, String ssNum){
@@ -25,9 +40,94 @@ public class Employee {
         this.ssNumber=ssNum;
         Employee.employeeCount++;
     }
+
+    //Getters & Setters
+    public static int getEmployeeCount() {
+        return employeeCount;
+    }
+
+    public static void setEmployeeCount(int employeeCount) {
+        Employee.employeeCount= employeeCount;
+    }
+
+    public String getSsNumber() {
+        return ssNumber;
+    }
+
+    public void setSsNumber(String ssNumber) {
+        this.ssNumber = ssNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(String hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public GENDER[] getGender(){
+        GENDER[] g=GENDER.values();
+        
+        return g;
+    }
     
-    
-    
-    
+    enum GENDER{M,F}
     
 }
--- src/concrete/HourlyEmployee.java
+++ src/concrete/HourlyEmployee.java
@@ -9,5 +9,74 @@ package concrete;
  * @author gcDataTechnology
  */
 public class HourlyEmployee extends Employee{
+
+     //different by Class
+    private String payPeriod="Weekly";
+    private double wage=7.75;
+    private double vacation=40.00;
+    private double bonus=0.00;//any employee can get a bonus, some are contracted
+    private final double EXPECTED_HOURS=40.0;
+    private double hoursWorked=40.0;
+    
+    
+    //Getters&Setters
+    public String getPayPeriod() {
+        return payPeriod;
+    }
+
+    public void setPayPeriod(String payPeriod) {
+        this.payPeriod = payPeriod;
+    }
+
+    public double getWage() {
+        return wage;
+    }
+
+    public void setWage(double wage) {
+        this.wage = wage;
+    }
+
+    public double getVacation() {
+        return vacation;
+    }
+
+    public void setVacation(double vacation) {
+        this.vacation = vacation;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+    
+    
+    
+    //Method
+    public String getPaid(){
+        String paid="";
+        double hrs=0.00;
+        
+        if(hoursWorked>EXPECTED_HOURS){
+            hrs=hoursWorked*wage;//standard wage
+            hrs+=(hoursWorked - EXPECTED_HOURS)*(wage/2);//calculate overtime
+        }else{
+            hrs=hoursWorked*wage;//standard wage
+        }
+            hrs+=bonus;
+            
+        paid="You've worked " + hoursWorked + " earning $" + hrs; 
+        return paid;
+    }
     
 }
